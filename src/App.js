30024986fb2fc0ecd0b218c4dd3227018f3a3bdc
Alex <oleksii.uzhva.pz.2021@lpnu.ua>
2024-03-23 12:08:49 +0200

    feat: lazy loading, check for data to be set before loading Lab2 components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { UserProvider } from './utils/UserProvider';
-import Lab1 from './pages/lab1/Lab1';
+// import Lab1 from './pages/lab1/Lab1';
 import Lab2 from './pages/lab2/Lab2';
 import Login from './pages/lab1/Login';
-import Graphs from './pages/lab1/Lab1Graphics';
+// import Graphs from './pages/lab1/Lab1Graphics';
+
+const LazyLab1 = lazy(() => import('./pages/lab1/Lab1'));
+const LazyLab2 = lazy(() => import('./pages/lab2/Lab2'));
+const LazyGrapghs = lazy(() => import('./pages/lab1/Lab1Graphics'))
+
 
 function App() {
   return (
@@ -12,10 +17,11 @@ function App() {
       <UserProvider>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Login />} />
-          <Route path="/lab1" element={<Lab1 />} />
-          <Route path="/graphs" element={<Graphs />} />
-          <Route path="/lab2" element={<Lab2 />} />
+          <Route path="/home" element={<Suspense fallback={<div>Loading...</div>}><LazyLab1 /></Suspense>} />
+          <Route path="/lab1" element={<Suspense fallback={<div>Loading...</div>}><LazyLab1 /></Suspense>}  />
+          <Route path="/graphs" element={<Suspense fallback={<div>Loading...</div>}><LazyGrapghs /></Suspense>} />
+          <Route path="/lab2" element={<Suspense fallback={<div>Loading...</div>}><LazyLab2 /></Suspense>} />
+          {/* <Suspense fallback={<div>Loading...</div>}><LazyLab2 /></Suspense> */}
         </Routes>
       </UserProvider>
     </Router>
--- src/pages/lab2/Lab2.js
+++ src/pages/lab2/Lab2.js
@@ -1,17 +1,13 @@
+import { useState, useEffect, useContext } from "react";
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import UserContext from '../../utils/UserProvider';
-import { useEffect, useContext } from "react";
 import Header from "../../utils/Header";
 import TopNavbar from "../../utils/TopNavbar";
 import NMTable from "./lab2Utils/NMTable";
-// import NMInputs from "./lab2Utils/NMInputs";
 import NMGraph2 from "./lab2Utils/NMGraph2";
 import NMInputsSimpleExperts from "./lab2Utils/NMInputsSimpleExperts";
 import NMInputsSimpleValues from "./lab2Utils/NMInputsSimpleValues";
 import NMInfo from "./lab2Utils/NMInfo";
-
-
-
 
 const Lab2 = () => {
     const expertEstimationsLocalHigh = [9, 7, 5, 3, 1, 1];
@@ -19,34 +15,33 @@ const Lab2 = () => {
     const tableValuesHigh = [60, 70 ,80, 90, 100, 110];
     const tableValuesLow = [110, 100 ,90, 80, 70, 60];
     const { expertEstimations, setExpertEstimations, lab2Values, setLab2Values } = useContext(UserContext);
+    const [loading, setLoading] = useState(true); // State to track loading
 
     useEffect(() => {
-        setExpertEstimations(expertEstimationsLocalHigh);
-        setLab2Values(tableValuesHigh);
-        console.log(expertEstimationsLocalHigh);
-        console.log(expertEstimations);
-        console.log(tableValuesHigh);
-        console.log(lab2Values);
-    }, [])
+        // Simulate asynchronous events
+        setTimeout(() => {
+            setExpertEstimations(expertEstimationsLocalHigh);
+            setLab2Values(tableValuesHigh);
+            setLoading(false); // Set loading to false after events are completed
+        }, 1000); // Adjust delay as needed
+    }, []);
 
-    function handleClickSetHigh(){
+    function handleClickSetHigh() {
         setExpertEstimations(expertEstimationsLocalHigh);
         setLab2Values(tableValuesHigh);
-        console.log('Set high')
     }
 
-    function handleClickSetMedium(){
+    function handleClickSetMedium() {
         setExpertEstimations(expertEstimationsLocalHighMedium);
-        setLab2Values(tableValuesHigh)
-        console.log('Set medium')
+        setLab2Values(tableValuesHigh);
     }
 
-    function handleClickSetLow(){
-        setExpertEstimations(expertEstimationsLocalHigh)
-        setLab2Values(tableValuesLow)
+    function handleClickSetLow() {
+        setExpertEstimations(expertEstimationsLocalHigh);
+        setLab2Values(tableValuesLow);
     }
 
-    return ( 
+    return (
         <div>
             <TopNavbar />
             <Header title='Нечіткі множини' />
@@ -67,41 +62,42 @@ const Lab2 = () => {
                             Низька Швидкість
                         </Button>
                     </Col>
-
                 </Row>
-                <Row className='d-flex justify-content-center'>
-                    <Col lg={3}>
-                        <NMInputsSimpleValues />
-                    </Col>
-                    <Col lg={3}>
-                    <NMInputsSimpleExperts values={tableValuesHigh}/>
-                    </Col>
-                    <Col lg={5}>
-                        <NMTable  values={tableValuesHigh} />
-                        {/* estimations={expertEstimations} */}
-                    </Col>
-                </Row>
+                {!loading && (
+                    <Row className='d-flex justify-content-center'>
+                        <Col lg={3}>
+                            <NMInputsSimpleValues />
+                        </Col>
+                        <Col lg={3}>
+                            <NMInputsSimpleExperts values={tableValuesHigh}/>
+                        </Col>
+                        <Col lg={5}>
+                            <NMTable  values={tableValuesHigh} />
+                        </Col>
+                    </Row>
+                )}
             </Container>
 
-            <Container>
-                <Row className='d-flex justify-content-center'>
-                    <Col lg={6}>
-                        <NMGraph2 values={tableValuesHigh}/>
-                        {/* estimations={expertEstimations} */}
-                    </Col>
-                </Row>
-            </Container>
-            
-            <Container className="mt-5">
-                <Row className='d-flex justify-content-center'>
-                    <Col lg={6}>
-                        <NMInfo />
-                        {/* estimations={expertEstimations} */}
-                    </Col>
-                </Row>
-            </Container>
+            {!loading && (
+                <>
+                    <Container>
+                        <Row className='d-flex justify-content-center'>
+                            <Col lg={6}>
+                                <NMGraph2 values={tableValuesHigh}/>
+                            </Col>
+                        </Row>
+                    </Container>
+                    <Container className="mt-5">
+                        <Row className='d-flex justify-content-center'>
+                            <Col lg={6}>
+                                <NMInfo />
+                            </Col>
+                        </Row>
+                    </Container>
+                </>
+            )}
         </div>
-     );
+    );
 }
- 
+
 export default Lab2;
